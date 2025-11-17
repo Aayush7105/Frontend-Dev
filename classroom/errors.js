d2329c3fc99e802c5ec4a017412f8d99ac8edb83
Aayush7105 <aayushrawat5107@gmail.com>
2025-11-17 19:00:05 +0530

    errors in javascript

---
+++ classroom/errors.js
@@ -0,0 +1,15 @@
+//Errors
+//type Error
+// let d = 45;
+// d.toLowerCase();
+
+// Reference error
+// console.log("%%ZZZZ");
+
+d();
+d();
+const d = function sub() {
+  console.log(10 - 10);
+};
+console.log(f);
+let f = 5;
